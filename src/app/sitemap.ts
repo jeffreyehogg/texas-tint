4e6b4fc7600cefa4bbd2befc751705a16a1120b9
Jeff Hogg <jeffhogg86@gmail.com>
2024-09-25 13:44:59 -0500

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://texastint.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://texastint.com/services',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://texastint.com/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ]
+}
