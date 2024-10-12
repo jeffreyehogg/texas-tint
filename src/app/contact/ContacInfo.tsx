c2052bb0a4d59a6ffcbaf2a2b46b91b7a7b2dfa5
Jeff Hogg <jeffhogg86@gmail.com>
2024-10-12 10:18:05 -0500

    add store hours

--- src/app/contact/ContacInfo.tsx
+++ src/app/contact/ContacInfo.tsx
@@ -1,10 +1,10 @@
 import Container from '@/components/Container'
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/solid'
+import { EnvelopeIcon, PhoneIcon, ClockIcon } from '@heroicons/react/24/solid'
 
 export const ContactInfo = () => (
   <Container>
     <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-      <div className="lg:order-first lg:row-span-2">
+      <div className="mr-8 lg:order-first lg:row-span-2">
         <h2 className="text-4xl font-extrabold text-blue-800 dark:text-white">
           Contact Us
         </h2>
@@ -22,7 +22,6 @@ export const ContactInfo = () => (
             >
               office@texastint.com{' '}
             </a>
-            {/* space */}
             or by phone at{' '}
             <a
               href="tel:+1-832-363-5100"
@@ -34,18 +33,24 @@ export const ContactInfo = () => (
           </p>
         </div>
       </div>
-      <div className="space-y-8">
-        <ul role="list">
-          <div className="mt-8 flex justify-center space-x-4 border-t border-zinc-100 pt-8 dark:border-zinc-700/40">
+      <div className="space-y-8 lg:order-last lg:row-span-1">
+        <ul role="list" className="space-y-8">
+          <div className="m-8 flex space-x-4 border-t border-zinc-100 pt-8 dark:border-zinc-700/40">
+            <ClockIcon className="h-6 w-6 flex-none fill-blue-500" />
+            <span className="group flex text-sm font-medium text-zinc-800 transition dark:text-zinc-200">
+              Mon - Sat: 9:00 AM - 5:00 PM
+            </span>
+          </div>
+          <div className="m-8 flex space-x-4 border-t border-zinc-100 pt-8 dark:border-zinc-700/40">
             <EnvelopeIcon className="h-6 w-6 flex-none fill-blue-500" />
             <a
-              href="tel:+1-832-363-5100"
+              href="mailto:office@texastint.com"
               className="group flex text-sm font-medium text-zinc-800 transition hover:text-[#00205A] dark:text-zinc-200 dark:hover:text-[#00205A]"
             >
               office@texastint.com
             </a>
           </div>
-          <div className="mt-8 flex justify-center space-x-4 border-t border-zinc-100 pt-8 dark:border-zinc-700/40">
+          <div className="m-8 flex space-x-4 border-t border-zinc-100 pt-8 dark:border-zinc-700/40">
             <PhoneIcon className="h-6 w-6 flex-none fill-blue-500" />
             <a
               href="tel:+1-832-363-5100"
